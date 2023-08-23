250bb97e5bf530738a540462a5d7dfe54b34d1c3
reifutnico <129698649+reifutnico@users.noreply.github.com>
2023-08-22 23:03:32 -0300

    22/8

---
+++ TP07_PreguntadORT2-master/Models/Dificultad.cs
@@ -0,0 +1,8 @@
+public class Dificultad
+{
+    public int IdDificultad{get;set;}
+    public string Nombre{get;set;}
+    static public int dificultadSeleccionada {get;set;}
+    static Dificultad(){}
+
+}
